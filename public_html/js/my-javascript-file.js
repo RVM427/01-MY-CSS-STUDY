a4f71ff54faa31f40161e84405abacf5dc009f8a
Your Name <you@example.com>
2019-08-14 17:00:47 +0530

    06 Timing parser  blocking

--- public_html/js/my-javascript-file.js
+++ public_html/js/my-javascript-file.js
@@ -1,9 +1,33 @@
+
+ var i=0;
+ var loop=true;
 function functionName(){
 	alert('Hello world');
 }
-/*
- while(true){
-             console.log("hey");
-         }
+ $(document).keypress(function(event) {
+      console.log("hey 1"+loop);
+     loop=false;
+     console.log("hey 2"+loop);
+});
+
+
+function times()
+{
+    setTimeout(function(){
+
+           console.log("Loop "+loop+"::"+ i++);
+           
+           if(loop)
+           times();
+},1000); 
+}
+
+ //times();
  
- */
+ //while(loop);
+ //console.log("loop done");
+ 
+ 
+  
+ 
+ 
